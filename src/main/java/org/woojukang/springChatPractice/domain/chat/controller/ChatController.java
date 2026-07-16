66cee18e2dd4948ee87837ddfd1fc58979af927a
KangWooJu <space1102@naver.com>
2026-07-17 02:06:17 +0900

    refactor : add method signature with @payload,Principal object with using facade class's method

--- src/main/java/org/woojukang/springChatPractice/domain/chat/controller/ChatController.java
+++ src/main/java/org/woojukang/springChatPractice/domain/chat/controller/ChatController.java
@@ -5,16 +5,29 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
-import org.woojukang.springChatPractice.domain.chat.dto.ChatDTO;
+import org.woojukang.springChatPractice.domain.chat.dto.request.SendChatMessageRequest;
+import org.woojukang.springChatPractice.domain.chat.facade.ChatFacade;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 @MessageMapping("/api/v1/chat")
 public class ChatController {
 
+    private final ChatFacade chatFacade;
+
     @MessageMapping("/{roomId}/send")
     public void sendMessage(@DestinationVariable Long roomId,
-                            @Payload ChatDTO chatDTO){
+                            @Payload SendChatMessageRequest request,
+                            Principal principal){
+
+        chatFacade
+                .publishMessage(
+                        roomId,
+                        request,
+                        principal
+                                .getName());
 
     }
 
