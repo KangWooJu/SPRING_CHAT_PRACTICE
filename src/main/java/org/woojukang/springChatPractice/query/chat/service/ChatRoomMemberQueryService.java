d07a4fdf0643b5d5a69f4ab8eeba07b0bd4115cf
KangWooJu <space1102@naver.com>
2026-07-17 21:30:06 +0900

    feat : add queryDSL method for validating subscriber with roomId

--- src/main/java/org/woojukang/springChatPractice/query/chat/service/ChatRoomMemberQueryService.java
+++ src/main/java/org/woojukang/springChatPractice/query/chat/service/ChatRoomMemberQueryService.java
@@ -2,6 +2,7 @@ package org.woojukang.springChatPractice.query.chat.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.woojukang.springChatPractice.domain.chat.entity.ChatRoomMember;
 import org.woojukang.springChatPractice.query.chat.repository.ChatRoomMemberQueryRepository;
 
@@ -14,5 +15,13 @@ public class ChatRoomMemberQueryService {
     public ChatRoomMember findByRoomId(Long roomId){
 
         return chatRoomMemberQueryRepository.findByRoomId(roomId);
+    }
+
+    // 단일 메소드 (facade사용 금지)
+    @Transactional(readOnly = true)
+    public boolean checkSubscriberWithRoomId(Long roomId,Long userId){
+
+        return chatRoomMemberQueryRepository.checkSubscriberWithRoomId(roomId,userId);
+
     }
 }
