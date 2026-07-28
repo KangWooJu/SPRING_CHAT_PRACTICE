88f313e1861f43f3152f74621aa7cea6838c8b33
KangWooJu <space1102@naver.com>
2026-07-28 16:40:04 +0900

    feat : add query service method to find ChatRoomMember entity by memberId column

--- src/main/java/org/woojukang/springChatPractice/query/chat/service/ChatRoomMemberQueryService.java
+++ src/main/java/org/woojukang/springChatPractice/query/chat/service/ChatRoomMemberQueryService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.woojukang.springChatPractice.domain.chat.entity.ChatRoomMember;
+import org.woojukang.springChatPractice.global.config.exception.WebSocketExceptionEnum;
+import org.woojukang.springChatPractice.global.config.exception.domain.BaseException;
 import org.woojukang.springChatPractice.query.chat.repository.ChatRoomMemberQueryRepository;
 
 import java.util.List;
@@ -22,6 +24,14 @@ public class ChatRoomMemberQueryService {
     public List<ChatRoomMember> findByRoomId(Long roomId){
 
         return chatRoomMemberQueryRepository.findByRoomId(roomId);
+    }
+
+    public ChatRoomMember findByMemberId(Long memberId){
+
+        return chatRoomMemberQueryRepository
+                .findByMemberId(memberId)
+                .orElseThrow(()-> new BaseException(WebSocketExceptionEnum
+                        .SUBSCRIBER_NOT_MATCHED));
     }
 
     // 단일 메소드 (facade사용 금지)
